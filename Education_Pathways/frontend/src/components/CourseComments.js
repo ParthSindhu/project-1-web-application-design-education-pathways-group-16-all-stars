922935c4bab696c96724abf37829d59ca13f8007
Sepehr <sepehr79h@gmail.com>
2022-10-30 19:01:55 -0400

    add form for user to submit comment
    Signed-off-by: Sepehr <sepehr79h@gmail.com>

--- Education_Pathways/frontend/src/components/CourseComments.js
+++ Education_Pathways/frontend/src/components/CourseComments.js
@@ -1,4 +1,6 @@
 import CourseDescriptionPage from "./CourseDescription";
+import './css/Comments.css'
+import React from "react";
 
 class CourseComments extends CourseDescriptionPage{
     constructor(props) {
@@ -7,10 +9,32 @@ class CourseComments extends CourseDescriptionPage{
     componentDidMount() {
         console.log("pass in course code: ", this.props.match.params.code)
     }
+    submitComment = () => {
+        console.log("submit comment")
+    }
 
     render() {
         return (
-            <div> Course Comments Page </div>
+            <div id="respond">
+
+                <h3> Leave a Comment</h3>
+                <form id="commentform">
+                    <label htmlFor="comment_author" className="required">Your name</label>
+                    <input type="text" name="comment_author" id="comment_author" tabIndex="1"
+                           required="required" maxLength="20"/>
+
+                    <label htmlFor="email" className="required">Your email</label>
+                    <input type="email" name="email" id="email" tabIndex="2" required="required" maxLength="320"/>
+
+                    <label htmlFor="comment" className="required">Your message</label>
+                    <textarea name="comment" id="comment" rows="7" tabIndex="4" maxLength="500" required="required"/>
+
+                    <input type="hidden" name="comment_post_ID" value="1" id="comment_post_ID" />
+                    <button className={"syllabus-link"} onClick={this.submitComment}>Submit</button>
+
+                </form>
+
+            </div>
         );
     }
 }
