bc33ec635c48055c3fbd0a1c0d525461424ac5f3
Parth Sindhu <sindhu.parth@gmail.com>
2022-11-11 22:54:20 -0500

    connected comments to backend GET, POSt endpoints

--- Education_Pathways/frontend/src/components/CourseComments.jsx
+++ Education_Pathways/frontend/src/components/CourseComments.jsx
@@ -2,15 +2,56 @@ import CourseDescriptionPage from "./CourseDescription";
 import './css/Comments.css'
 import React from "react";
 import Comment from "./Comment";
+import axios from "axios";
 
 class CourseComments extends CourseDescriptionPage{
+    constructor(props) {
+        super(props);
+        this.state = {
+            comments: []
+        };
+    }
     
     componentDidMount() {
         console.log("pass in course code: ", this.props.match.params.code)
     }
-    submitComment = () => {
-        console.log("submit comment")
+    submitComment = (event) => {
+        event.preventDefault();
+        let url = "http://localhost:5000"
+        console.log(event)
+        const data = new FormData(event.currentTarget);
+
+        let text = data.get("comment");
+        let username = data.get("comment_author");
+        let email = data.get("email");
+        console.log(text, username, email);
+        let course = this.props.match.params.code;
+        axios.post(`${url}/course/comments`, {
+            text,
+            username,
+            course
+        })
+        .then(res => {
+            console.log(res.data)
+            this.setState({comments: res.data.comments})
+        })
+        .catch(err => {
+            console.log(err)
+        })
     }
+
+
+    componentDidUpdate() {
+        // fetch comments from backend
+        fetch("http://localhost:5000/course/comments?course=" + this.props.match.params.code).then(
+            response => response.json()
+        ).then(comments => {
+            this.setState({comments: comments.comments});
+        }).catch(
+            error => console.log(error)
+        )
+    }
+    
 
     render() {
         return (
@@ -19,19 +60,22 @@ class CourseComments extends CourseDescriptionPage{
                 <div className="container">
                             <h3 className="comments-title">Comments</h3>
                 <ul className="comments-list">
+                    { 
+                     this.state.comments?.map((comment, index) =>  
                     <Comment 
                     comment={{
-                        userId : "parth",
-                        username : "parth",
-                        body : "heelo",
-                        createdAt : new Date(),
+                        username : comment.username,
+                        body : comment.text,
+                        createdAt : comment.timestamp.$date,
                     }}    
                     />
+                     )
+                     }
                 </ul> 
                 </div>
                 <div>
                     <h3> Leave a Comment</h3>
-                    <form id="commentform">
+                    <form id="commentform" onSubmit={this.submitComment}>
                         <label htmlFor="comment_author" className="required">Your name</label>
                         <input type="text" name="comment_author" id="comment_author" tabIndex="1"
                             required="required" maxLength="20"/>
@@ -43,7 +87,7 @@ class CourseComments extends CourseDescriptionPage{
                         <textarea name="comment" id="comment" rows="7" tabIndex="4" maxLength="500" required="required"/>
 
                         <input type="hidden" name="comment_post_ID" value="1" id="comment_post_ID" />
-                        <button className={"syllabus-link"} onClick={this.submitComment}>Submit</button>
+                        <button className={"syllabus-link"} >Submit</button>
 
                     </form>
                 </div>
