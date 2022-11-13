e66353b25a4e6aeb8afd1a198dc7d59436f2e1f2
NathanJones98 <59748950+NathanJones98@users.noreply.github.com>
2022-11-12 23:14:35 -0500

    Update

--- Education_Pathways/frontend/src/components/Navbar.js
+++ Education_Pathways/frontend/src/components/Navbar.js
@@ -9,7 +9,6 @@ import CourseDescriptionPage from "./CourseDescription";
 // import Wishlist from './Wishlist';
 // import SignUp from './SignUp'
 import SearchResultDisplay from './ResultDisplay'
-import CourseComments from "./CourseComments";
 
 function CourseDescription (props) {
   let query = useQuery();
@@ -49,7 +48,6 @@ export default class NavbarComp extends Component {
       <Router>
         <div>
           <Navbar className = "navbar" variant="dark" sticky="top" expand="lg">
-          <Navbar className = "navbar" variant="dark" sticky="top" expand="lg">
             <Navbar.Brand>
               <img src={logo} alt="" />{" "}
               <Nav.Link href="/" style={{ color: "white", display: "inline" }}>
@@ -60,12 +58,8 @@ export default class NavbarComp extends Component {
             <Navbar.Toggle />
             <Navbar.Collapse>
               <Nav className = 'nav-item a'>
-              <Nav className = 'nav-item a'>
                 <Nav.Link as={Link} to="/about">
                   About Us
-                </Nav.Link>
-                <Nav.Link as={Link} to="/test">
-                  Test
                 </Nav.Link>
                 <Nav.Link as={Link} to="/test">
                   Test
@@ -74,7 +68,6 @@ export default class NavbarComp extends Component {
                 {/* <Nav.Link href="/search" style={{ color: "white", display: "inline" }}>
                   Search
                 </Nav.Link> */}
-
 
               </Nav>
             </Navbar.Collapse>
@@ -86,12 +79,7 @@ export default class NavbarComp extends Component {
               <div className = "body_text">
               <p> 
                 Welcome to CARTE's in-development tool for course selection at UofT. Education Pathways allows for more intelligent course searching, by matching not just the terms you search, but ones relevant to them. The more terms you search for, the more relevant your results will be! Even try searching across disciplines for the courses that best cover each.
-            <Route path="/about">
-              <div className = "body_text">
-              <p> 
-                Welcome to CARTE's in-development tool for course selection at UofT. Education Pathways allows for more intelligent course searching, by matching not just the terms you search, but ones relevant to them. The more terms you search for, the more relevant your results will be! Even try searching across disciplines for the courses that best cover each.
 
-                Whatever year you are looking for, Education Pathways will also suggest courses in earlier years that will best help you to prepare. To get the most out of this, try searching for courses in a later year and see what is suggested for your current one.
                 Whatever year you are looking for, Education Pathways will also suggest courses in earlier years that will best help you to prepare. To get the most out of this, try searching for courses in a later year and see what is suggested for your current one.
 
                 We are looking for feedback to improve Education Pathways and make it more useful for students. If you have ideas or suggestions, please <a href = "mailto:alex.olson@utoronto.ca">  email us! </a> <br></br>
@@ -101,16 +89,7 @@ export default class NavbarComp extends Component {
               </p>
               <p>Alexander Olson <a href="https://carte.utoronto.ca/"> (CARTE)</a> </p>
               <p>Student team from <a href="https://shuiblue.github.io/UofT-ECE444/">ECE444-Fall2021</a> : Janelle Cuevas, Jean Lin, Terry Luan, Cansin Varol, Nick Woo</p>
-                We are looking for feedback to improve Education Pathways and make it more useful for students. If you have ideas or suggestions, please <a href = "mailto:alex.olson@utoronto.ca">  email us! </a> <br></br>
-              </p>
-              <p> 
-                <b>Development Team: </b>
-              </p>
-              <p>Alexander Olson <a href="https://carte.utoronto.ca/"> (CARTE)</a> </p>
-              <p>Student team from <a href="https://shuiblue.github.io/UofT-ECE444/">ECE444-Fall2021</a> : Janelle Cuevas, Jean Lin, Terry Luan, Cansin Varol, Nick Woo</p>
 
-                  
-              </div>
                   
               </div>
               {/* <SearchResultDisplay /> */}
@@ -122,15 +101,8 @@ export default class NavbarComp extends Component {
               <SearchResultDisplay />
             </Route>
             <Route exact
-              path="/courseComments/:code"
-              render={props =>(<CourseComments {...props} />)}>
-            </Route>
-            <Route path="/test">
-              <SearchResultDisplay />
-            </Route>
-            <Route exact
-              path="/courseComments/:code"
-              render={props =>(<CourseComments {...props} />)}>
+              path="/courseDetails/:code"
+              render={props =>(<CourseDescriptionPage {...props} />)}>
             </Route>
             <Route path="/">
               <SearchResultDisplay />
