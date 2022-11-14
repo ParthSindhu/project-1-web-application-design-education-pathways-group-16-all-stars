d2b43ea9613011b6f1c934ba68686a6477426e41
Sepehr <sepehr79h@gmail.com>
2022-11-13 20:42:34 -0500

    fix build warnings
    Signed-off-by: Sepehr <sepehr79h@gmail.com>

--- Education_Pathways/frontend/src/components/SticktoTop.js
+++ Education_Pathways/frontend/src/components/SticktoTop.js
@@ -1,10 +1,10 @@
-import React, {useState, useEffect, useRef, math} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 
 
 
 const SticktoTop = (props) => {
 
-    const [bottom_pos, setBottomPos] = useState(0);
+    const [, setBottomPos] = useState(0);
 
     const reg_style ={
         color:'white',
@@ -31,36 +31,35 @@ const SticktoTop = (props) => {
         height: '0px',
     };
 
-    const [scrollPosition, setScrollPosition] = useState(0);
+    const [, setScrollPosition] = useState(0);
     const [style, setStyle] = useState(reg_style);
     const [style_fill, setStyleFill] = useState(reg_style);
     const [ret_top, setRetTop] = useState(0);
 
     const inputRef = useRef();
 
-    const handleScroll = () => {
-        console.log(inputRef.current.getBoundingClientRect());
-        const top_ = inputRef.current.getBoundingClientRect().y;
-        setBottomPos(inputRef.current.getBoundingClientRect().height);
-
-        const position = window.pageYOffset;
-        setScrollPosition(position);
-        
-        if(Math.floor(top_) <= 70 && (position > ret_top )){
-            setStyle(fixed_style);
-            setRetTop(position);
-            setStyleFill(filler);
-        } else {
-            setStyle(reg_style);
-            setStyleFill(filler_none);
-            setRetTop(0);
-        }
-    };
+    
 
     useEffect(() => {
+        const handleScroll = () => {
+            console.log(inputRef.current.getBoundingClientRect());
+            const top_ = inputRef.current.getBoundingClientRect().y;
+            setBottomPos(inputRef.current.getBoundingClientRect().height);
+    
+            const position = window.pageYOffset;
+            setScrollPosition(position);
+            
+            if(Math.floor(top_) <= 70 && (position > ret_top )){
+                setStyle(fixed_style);
+                setRetTop(position);
+                setStyleFill(filler);
+            } else {
+                setStyle(reg_style);
+                setStyleFill(filler_none);
+                setRetTop(0);
+            }
+        };
         window.addEventListener('scroll', handleScroll, { passive: true });
-
-
         return () => {
             window.removeEventListener('scroll', handleScroll);
         };
