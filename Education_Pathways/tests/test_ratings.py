6f4b5bfae1558c49d4d7f8f37c9f0a2c8a1218b1
Sepehr <sepehr79h@gmail.com>
2022-11-14 17:43:47 -0500

    remove unused comments
    Signed-off-by: Sepehr <sepehr79h@gmail.com>

--- Education_Pathways/tests/test_ratings.py
+++ Education_Pathways/tests/test_ratings.py
@@ -1,7 +1,4 @@
-import pytest
 from index import app
-from minor import check_course_in_minor
-from flask.testing import FlaskClient
 
 def test_course_ratings_get_endpoint():
     tester = app.test_client()
