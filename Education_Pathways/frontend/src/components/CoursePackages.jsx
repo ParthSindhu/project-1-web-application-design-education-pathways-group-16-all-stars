0a1d166064ca4b925c9da95f7ae27b31f7e79228
Parth Sindhu <sindhu.parth@gmail.com>
2022-11-15 15:45:15 -0500

    Added basic form and post request

--- Education_Pathways/frontend/src/components/CoursePackages.jsx
+++ Education_Pathways/frontend/src/components/CoursePackages.jsx
@@ -3,6 +3,36 @@ import CoursePackage from './CoursePackage';
 
 import './css/coursepackages.css'
 
+function submitPackage(event){
+  console.log("submit package")
+  event.preventDefault();
+  const data = new FormData(event.currentTarget);
+  let name = data.get("package");
+  let description = data.get("description");
+  let course1 = data.get("p1");
+  let course2 = data.get("p2");
+  let course3 = data.get("p3");
+  let course4 = data.get("p4");
+  let courses = [course1, course2, course3, course4];
+  fetch(`${process.env.REACT_APP_API_URL}/packages`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name,
+      description,
+      courses
+    }),
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+  }
+  )
+}
+
+
 export default function CoursePackages() {
   // fetch data from packages endpoint
   const [data, setData] = React.useState([]);
@@ -41,6 +71,7 @@ export default function CoursePackages() {
       }
   }, [query]);
   return (
+    <>
     <div className='course-packages'>
       <h1>Course Packages</h1>
       <form onSubmit={(event)=>{
@@ -66,5 +97,26 @@ export default function CoursePackages() {
       </div>
       }
     </div>
+    <div className='submit-package-form'>
+    <h3> Create a New Package</h3>
+    <form id="packageform" onSubmit={submitPackage}>
+        <label htmlFor="package" className="required"> Add Package name</label>
+        <input type="text" name="package" tabIndex="1"
+            required="required" maxLength="20"/>
+        <br />
+        <label htmlFor="description" className="required"> Add Description</label> <br />
+        <textarea name="description" rows="7" tabIndex="4" maxLength="500" required="required"/>
+        <br />
+        Add Courses
+        <br />
+        Course 1<input type="text" name="p1"  /> <br />
+        Course 2<input type="text" name="p2"  /> <br />
+        Course 3<input type="text" name="p3"  /> <br />
+        Course 4<input type="text" name="p4"  /> <br />
+        <button className={"syllabus-link"} >Submit</button>
+
+    </form>
+</div>
+</>
   );
 }
